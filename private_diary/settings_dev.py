db16488f7aa12e4595b9f680b0304cb1fe9d716c
yoshimi-43 <irieyoshimi43+1@gmail.com>
2025-09-05 10:35:23 +0900

    設定ファイルの分割

---
+++ private_diary/settings_dev.py
@@ -0,0 +1,50 @@
+from .settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-#i)dedt^odzs2f$+_vy87hhbud9#gwgiif3a+#t1*@m(ne9z15'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# ロギング設定
+LOGGING = {
+  'version': 1,
+  'disable_existing_loggers': False,
+
+  # ロガーの設定
+  'loggers': {
+    'django': {
+      'handlers': ['console'],
+      'level': 'INFO'
+    },
+    'diary': {
+      'handlers': ['console'],
+      'level': 'DEBUG'
+    }
+  },
+
+  # ハンドラの設定
+  'handlers': {
+    'console': {
+      'level': 'DEBUG',
+      'class': 'logging.StreamHandler',
+      'formatter': 'dev'
+    }
+  },
+
+  # フォーマッタの設定
+  'formatters': {
+    'dev': {
+      'format': '\t'.join([
+        '%(asctime)s',
+        '[%(levelname)s]',
+        '%(pathname)s(Line:%(lineno)d)',
+        '%(message)s'
+      ])
+    }
+  }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
