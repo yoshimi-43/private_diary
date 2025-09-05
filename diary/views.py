bb46761bb33d6fd0f571c22bd8082aa2f8c7f7e2
yoshimi-43 <irieyoshimi43+1@gmail.com>
2025-09-05 14:44:41 +0900

    日記一覧表示機能

--- diary/views.py
+++ diary/views.py
@@ -1,16 +1,19 @@
 import logging
 
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import InquiryForm
+from .forms import InquiryForm, DiaryCreateForm
+from .models import Diary
 
 logger = logging.getLogger(__name__)
 
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
 
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
@@ -22,3 +25,14 @@ class InquiryView(generic.FormView):
         messages.success(self.request, 'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
+
+
+class DiaryListView(LoginRequiredMixin, generic.ListView):
+    model = Diary
+    template_name = 'diary_list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
+        return diaries
+
