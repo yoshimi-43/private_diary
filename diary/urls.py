374fcc653a3831ccd92e5d68a89056e1d3ced3ea
yoshimi-43 <irieyoshimi43+1@gmail.com>
2025-09-05 09:58:33 +0900

    ルーティングを追加

--- diary/urls.py
+++ diary/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from . import views
+
 
 app_name = 'diary'
 urlpatterns = [
-  path('', views.IndexView.as_view(), name="index")
+    path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
 ]
